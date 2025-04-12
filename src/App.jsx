981e759c2092810f7eb02ceb9a37abedc69709a7
Mayank joshi <147848494+Mayankjoshi670@users.noreply.github.com>
2025-04-12 13:09:47 +0530

    scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero"
+import Highlight from "./components/Highlight"
+import Navbar from "./components/Navbar" 
+
+const App = ()=>{
+  return (
+    < main  className="bg-black">
+  <Navbar/> 
+  <Hero/>
+  <Highlight/>
+      
+    </main>
+  )
+}
+
+export default App 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {appleImg} from '../utils'
+const Navbar = () => {
+  return (
+    <div>
+      <img src={appleImg} alt="logo" srcset="" />
+    </div>
+  )
+}
+
+export default Navbar
